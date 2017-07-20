9d919a421f601ef233ec2da8fafd32f37fffa237
khachkara <khachkara@gmail.com>
2017-07-20 08:36:26 -0400

    Была реализована возможность перевода блока текста.

--- yandex/main.py
+++ yandex/main.py
@@ -1,4 +1,9 @@
 from yandex_translator import get_translation
 
 
-print(*get_translation(input(':> '),'en')['text'])
+def main(text, lang):
+    with open('output.txt', 'w', encoding='utf-8') as f:
+        print(*get_translation(text, lang)['text'], file=f)
+
+if __name__ == '__main__':
+    main(open('input.txt', encoding='utf-8'), 'ru')
